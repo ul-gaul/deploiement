114bcccb097e389e21c50b325182bd35fe49e962
Maxime Guillemette <maxime_glmt@hotmail.ca>
2018-05-13 20:18:32 -0400

    ajout struct pour les logs

--- main_deploiement/sd_logger.cpp
+++ main_deploiement/sd_logger.cpp
@@ -3,6 +3,7 @@
 void init_sd_logger(sdlogger_handle* sdlogger, byte chip_select_pin, 
                     char* logfilename) {
     SD.begin(chip_select_pin);
+    sdlogger->cspin = chip_select_pin;
     // generate filename, credit goes to Jonathan Neault for the algorithm
     for(int i = 1; i <= LOG_UNIT_MAX_NB_OF_FILES; i++) {
         sprintf(sdlogger->logfilename, "%s_%d%s", logfilename, i, 
@@ -19,3 +20,14 @@ void init_sd_logger(sdlogger_handle* sdlogger, byte chip_select_pin,
     sdlogger->file_handle.println(header);
 }
 
+void log_data(sdlogger_handle* logger, sd_log* log) {
+    
+    logger->file_handle.println(String(log->logbuffer));
+    logger->file_handle.flush();
+}
+
+void log_event(sdlogger_handle* logger, sd_log* log, String message) {
+    
+    logger->file_handle.println(String(log->logbuffer));
+    logger->file_handle.flush();
+}
--- main_deploiement/sd_logger.h
+++ main_deploiement/sd_logger.h
@@ -16,5 +16,16 @@ typedef struct {
 } sdlogger_handle;
 
 
+typedef struct {
+    char logbuffer[512];
+    float raw_altitude;
+    float filtered_altitude;
+    float speed;
+} sd_log;
+
 void init_sd_logger(sdlogger_handle* sdlogger, byte chip_select_pin, 
                     char* logfilename);
+
+void log_data(sdlogger_handle* logger, sd_log* log);
+
+void log_event(sdlogger_handle* logger, sd_log* log, String message);
