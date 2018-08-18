0c98abe21c6892a543215b9e4f3063c734ed0cef
Maxime Guillemette <maxime_glmt@hotmail.ca>
2018-08-18 13:05:24 -0400

    ajout params par défaut pour config déploiment

--- gaul_deploy/code_generator/deployment_generator.py
+++ gaul_deploy/code_generator/deployment_generator.py
@@ -3,8 +3,6 @@
 
 # misc imports
 from jinja2 import Environment, FileSystemLoader
-# template generators imports
-from deployment_configuration import DeploymentConfiguration
 
 
 j2_env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True)
@@ -35,4 +33,20 @@ deploy_config = GenericTemplate(j2_env.get_template(
 deploy_config['buzzer_time_between_sequences'] = 4000
 deploy_config['buzzer_cycle_duration'] = 500
 deploy_config['altitude_filter_order'] = 3
+# sampling period is in micro-seconds
+deploy_config['data_sampling_period'] = 100000
+# speeds are in m/s but must be converted to m/sample in the config file
+# for example, if the sampling period is 100000 us (10 Hz), then the converted
+# speed value will be 3 (3 m/0.1s = 30 m/s)
+deploy_config['breakpoint_speed_to_burnout'] = 30
+deploy_config['breakpoint_speed_to_pre_drogue'] = 30
+deploy_config['breakpoint_speed_to_idle'] = 0.1
+# altitudes are in meters
+deploy_config['breakpoint_altitude_to_burnout'] = 6
+deploy_config['breakpoint_altitude_to_drift'] = 460
+# number of samples (at sample rate) between apogee and drogue deployment
+deploy_config['breakpoint_delta_time_apogee'] = 4
+# expected altitude range in meters
+deploy_config['flight_maximal_altitude'] = 0
+deploy_config['flight_minimal_alitude'] = 4000
 deploy_config.to_file('test.out')
