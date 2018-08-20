b9a4da1677082980e8117154ca2a325ff7e6bd92
Maxime Guillemette <maxime_glmt@hotmail.ca>
2018-08-19 20:00:12 -0400

    ajout generation du code

--- gaul_deploy/cli.py
+++ gaul_deploy/cli.py
@@ -15,10 +15,24 @@ if os.name == 'nt':
 	clear = lambda: os.system('cls')
 
 
+class MenuSpecialExit(Exception):
+
+	def  __init__(self, exit_num):
+		self.exit_num = exit_num
+		super().__init__()
+
+
 class BaseMenu:
+	# exit codes for menu calls
+	EXIT_OK = 0
+	RUN_MENU_AGAIN = 1
+	HELP = 2
+	EXIT = 3
+	BACK = 4
 
 	def __init__(self, description):
 		self.description = description
+		self.error_message = None
 
 	def __str__(self):
 		return f'BaseMenu({self.description})'
@@ -26,24 +40,34 @@ class BaseMenu:
 	def __call__(self):
 		print('-' * 80)
 		print(self.description)
+		if self.error_message:
+			print(self.error_message)
 		print('-' * 80)
 
-	def display_help(self):
+	def user_input(self):
+		choice = input()
+		if choice == 'exit':
+			raise MenuSpecialExit(self.EXIT)
+		elif choice == 'help':
+			raise MenuSpecialExit(self.HELP)
+		elif choice == 'back':
+			raise MenuSpecialExit(self.BACK)
+		else:
+			return choice
+
+	def set_error_message(self, msg):
+		assert isinstance(msg, str)
+		self.error_message = msg
+
+
+class HelpMenu(BaseMenu):
+
+	def __call__(self):
+		super().__call__()
 		print('Enter \'exit\' at any time to exit')
 		print('Enter \'help\' at any time to display this message')
 		print('Enter \'back\' at any time to return to the previous menu')
-		input()
-
-	def make_choice(self):
-		choice = input()
-		if choice == 'exit':
-			sys.exit()
-		elif choice == 'help':
-			self.display_help()
-		elif choice == 'back':
-			return
-		else:
-			return choice
+		self.user_input()
 
 
 class MainMenu(BaseMenu):
@@ -54,7 +78,7 @@ class MainMenu(BaseMenu):
 		for i, menu in enumerate(self.menus):
 			print(i, ' - ', menu.description)
 		try:
-			choice = self.make_choice()
+			choice = self.user_input()
 			if not choice: return
 			next_menu = self.menus[int(choice)]
 		except (IndexError, ValueError):
@@ -67,7 +91,6 @@ class MainMenu(BaseMenu):
 class ExitMenu(BaseMenu):
 
 	def __call__(self):
-		# super().__call__()
 		sys.exit()
 
 
@@ -80,9 +103,10 @@ class FlightConfigMenu(BaseMenu):
 	def __call__(self):
 		super().__call__()
 		self.list_params()
-		choice = self.make_choice()
+		choice = self.user_input()
 		if not choice: return
 		self.edit_param(int(choice))
+		return self.RUN_MENU_AGAIN
 
 	def list_params(self):
 		print('Choose which parameter to edit:')
@@ -94,7 +118,7 @@ class FlightConfigMenu(BaseMenu):
 	def edit_param(self, index):
 		param = self.params[index][0]
 		print('Enter the new value for:', param)
-		new_value = input()
+		new_value = self.user_input()
 		self.deploy_config[param] = new_value
 
 
@@ -106,7 +130,16 @@ class GenerateCodeMenu(BaseMenu):
 
 	def __call__(self):
 		super().__call__()
-		input('generate code!')
+		print('The following files will be created:')
+		print(self.deploy_config.name)
+		print('Enter the path to the output file:')
+		path_file = self.user_input()
+		try:
+			self.deploy_config.to_file(path_file)
+		except FileNotFoundError:
+			print('Invalid directory/path:', path_file)
+			return self.RUN_MENU_AGAIN
+
 
 
 def main():
@@ -117,11 +150,20 @@ def main():
 	edit_flight_conf = FlightConfigMenu(deploy_config,
 		"Edit the flight's parameters")
 	generate_code = GenerateCodeMenu(deploy_config, 'Generate the code')
+	help_menu = HelpMenu('Help')
 	exit_menu = ExitMenu('Exit')
-	main_menu.menus = [edit_flight_conf, generate_code, exit_menu]
+	main_menu.menus = [edit_flight_conf, generate_code, help_menu, exit_menu]
 	while True:
 		clear()
-		main_menu()
+		try:
+			main_menu()
+		except MenuSpecialExit as e:
+			if e.exit_num == BaseMenu.HELP:
+				clear()
+				help_menu()
+			elif e.exit_num == BaseMenu.EXIT:
+				exit_menu()
+
 
 if __name__ == '__main__':
 	main()
